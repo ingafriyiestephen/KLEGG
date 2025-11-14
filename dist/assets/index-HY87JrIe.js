bc61ac33e3b192b9b92714a70ca2431cb738b77f
ingafriyiestephen <afriyiestephen225@yahoo.com>
2025-11-14 13:39:35 +0000

    Just ensuring the latest code

--- dist/assets/index-HY87JrIe.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/web-Dejp46rG.js","assets/index-CbFFUppU.js","assets/index-BmoP3781.css"])))=>i.map(i=>d[i]);
-import{ae as t,af as o}from"./index-CbFFUppU.js";const r=t("Toast",{web:()=>o(()=>import("./web-Dejp46rG.js"),__vite__mapDeps([0,1,2])).then(e=>new e.ToastWeb)});export{r as T};
---
+++ dist/assets/index-legacy-DhEKgVkm.js
@@ -0,0 +1,1 @@
+System.register(["./index-legacy-C8AAjeca.js"],function(e,t){"use strict";var n,r;return{setters:[function(e){n=e.ae,r=e.af}],execute:function(){e("T",n("Toast",{web:function(){return r(function(){return t.import("./web-legacy-7wsK3RKY.js")},void 0).then(function(e){return new e.ToastWeb})}}))}}});
